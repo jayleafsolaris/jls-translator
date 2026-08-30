29886773a379fde3814125c1e199dae17e688369
jayleafsolaris <jayleafyt@gmail.com>
2026-08-29 22:42:21 -0500

    Syncronized

--- translator/translator/modes/view.py
+++ translator/translator/modes/view.py
@@ -22,5 +22,4 @@ def cmd_view():
          print(f"{p.name:<16}{_human_size(size):>10}   {key_count}{marker}")
     if base_path.exists():
         count = get_update_count()
-        print(f"\n--update count for this base file: {count}/{DEFAULTS['update_limit']}")
-
+        print(f"\nUpdate count for this base file: {count}/{DEFAULTS['update_limit']}")
